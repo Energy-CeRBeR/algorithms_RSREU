00c04239aa281e2b687156f679d3392cd6bc8e9d
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-10-22 21:33:32 +0300

    Solved all problems on binary search

--- binary_search/L.py
+++ binary_search/L.py
@@ -1,27 +1,30 @@
-def check_answer(mx_d):
-    count = 0
-    mn = 10 ** 9
-    mx = - 10 ** 9
-
-    length = 0
-    for i in range(n):
-        mx = max(mx, heights[i])
-        mn = min(mn, heights[i])
+def hook(coeff):
+    h = 0
+    u = 0
+    for i in range(len(diff)):
+        u -= 1
+        if u < 1 and diff[i] <= coeff:
+            h += 1
+            u = c
+    if h >= r:
+        return True
+    return False
 
 
 n, r, c = map(int, input().split())
-heights = list(int(input()) for _ in range(n))
-heights.sort()
+heights = sorted([int(input()) for _ in range(n)])
+diff = [heights[i + c - 1] - heights[i] for i in range(len(heights) - c + 1)]
 
-left = 0
-right = 10 ** 9
-while left <= right:
-    middle = (left + right) // 2
-    if check_answer(middle):
-        right = middle - 1
-    else:
-        right = middle = 1
+left = -1
+right = heights[-1] - heights[0]
+if r > 1:
+    while left + 1 < right:
+        middle = (left + right) // 2
+        if hook(middle):
+            right = middle
+        else:
+            left = middle
+    print(right)
 
-    print(left, right)
-
-print(left)
+else:
+    print(min(diff))
